fb715b688cbfd898719998c4e4c8928bded74db7
mhasilau <mikhail.hasilau@gmail.com>
2025-06-02 21:29:02 +0300

    lesson 11 js

---
+++ lesson11/index.js
@@ -0,0 +1,19 @@
+const str1 = 'some RANDOM STriNg.';
+const str2 = 'some RANDOM STriNg 2';
+//
+// console.log(str1.toLowerCase());
+// console.log(str1.toUpperCase());
+// console.log(str1.concat(str2, ' ', 'asdas', 234324, '3424'));
+// console.log(str1.repeat(5));
+// console.log(str1.toLowerCase().includes('random'));
+// console.log(str1.startsWith('some'));
+// console.log(str1.endsWith('STriNg'));
+// console.log(str1.replaceAll('s', '$'));
+// console.log(str2.replace(/s/gi, '$'));
+// console.log(str1.trimEnd());
+// console.log(str1.charAt(10));
+// console.log(str1.indexOf('s'));
+// console.log(str1.lastIndexOf('s'));
+// console.log(str1.slice(5, 11));
+// console.log(str1.substring(5, 11));
+console.log(str1.substr(5, 6));
