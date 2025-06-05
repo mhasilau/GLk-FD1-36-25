f59030fffc35251e7dd4c2452fe7e69174a2fbd9
mhasilau <mikhail.hasilau@gmail.com>
2025-06-05 21:28:29 +0300

    lesson 12

---
+++ lesson12/index.js
@@ -0,0 +1,169 @@
+// const age = 8;
+// if (age >= 18) {
+//   console.log('welcome');
+// } else {
+//   console.log('sorry');
+// }
+//
+// age >= 18 ? console.log('welcome') : console.log('sorry');
+
+
+// if (age > 18) {
+//   console.log('> 18');
+// } else if (age < 18) {
+//   console.log('< 18');
+// } else {
+//   console.log('= 18');
+// }
+
+// ==
+// ===
+
+// >=
+// <=
+
+// console.log(1 == '1');
+// console.log(1 === '1');
+
+// !=
+// !==
+
+// const guest = 'petya';
+// if (guest !== 'petya') {
+//   console.log('welcome');
+// }
+
+// !true = false
+// !false = true
+
+// %
+
+// console.log(3098 % 2);
+//
+// const age = 23; // >=18
+// const isHasChildren = true; // false
+// const isCriminal = false; // false
+// const height = 150; // < 200
+// const sport = true; // true
+// // && И
+// // || ИЛИ
+//
+// if (
+//   age >= 18 &&
+//   (isHasChildren === false ||
+//   isCriminal === false) &&
+//   height <= 200 &&
+//   sport === true
+// ) {
+//   console.log('u r hire');
+// } else {
+//   console.log('sorry');
+// }
+
+// const guest = 'mike';
+
+// switch (guest) {
+//   case 'nick':
+//   case 'mike':
+//     console.log('hello, bro');
+//     break;
+//   case 'jane':
+//     console.log('nice to see you');
+//     break;
+//   default:
+//     console.log('go back');
+//     break;
+// }
+//
+//
+//
+
+
+// const str = 'hgfhfg123jgjgh423423';
+// // const str = false;
+// console.log(str, typeof str);
+//
+// const num1 = Number(str);
+// console.log(num1, typeof num1);
+// const num2 = +str;
+// console.log(num2, typeof num2);
+// const num3 = parseInt(str);
+// console.log(num3, typeof num3);
+
+// const num = 1243523;
+// console.log(num, typeof num);
+//
+// const str1 = String(num);
+// console.log(str1, typeof str1);
+// const str2 = num.toString();
+// console.log(str2, typeof str2);
+// const str3 = num + '';
+// console.log(str3, typeof str3);
+//
+
+// const num = 0;
+// const str = '';
+//
+// const boolStr = Boolean(num);
+// const boolNum = !!(str);
+// console.log(boolStr, typeof boolStr);
+// console.log(boolNum, typeof boolNum);
+
+// if (num) {
+//
+// }
+
+
+// const arr =
+//   [1, 'str str', true, null, NaN, undefined, { a: 2 }, [1, 2, 3]];
+// console.log(arr[7][0]);
+// const inArr = arr[7];
+// console.log(inArr[0]);
+
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const arr2 = [5, 6, 7, 8, 9];
+
+// console.log(arr.concat(arr2, ['1', '2', 3, '4'], 1, 2, 3));
+// console.log(arr.toReversed());
+// console.log(arr);
+//
+// const length = arr.push(9, 8, [7, 6], 5);
+// console.log(length);
+// console.log(arr);
+
+// const length = arr.unshift(9, 8, [7, 6], 5);
+// console.log(length);
+// console.log(arr);
+
+// const value = arr.pop();
+// console.log(value);
+// console.log(arr);
+
+// const value = arr.shift();
+// console.log(value);
+// console.log(arr);
+
+// console.log(arr.includes(3));
+
+// const arrSlice = arr.slice(4, 7);
+// console.log('arrSlice', arrSlice);
+// console.log(arr);
+//
+// const arrSplice =
+//   arr.splice(4, 0, 'four', 'five', 'six');
+// console.log('arrSplice', arrSplice);
+// console.log(arr);
+
+// const toSort = [3, 617, 65, 87, 15, 61, 25, 67, 68, 41, 6, 58, 7, 46];
+// console.log(toSort.toSorted((a, b) => a - b));
+// console.log(toSort);
+
+// const str = 'some random string';
+// const arr = str.split('');
+// console.log(arr);
+
+let userName = 'mikhail';
+const arr = userName.split('');
+arr[0] = arr[0].toUpperCase();
+userName = arr.join('');
+console.log(userName);
